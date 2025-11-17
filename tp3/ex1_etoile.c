c487dfbc2758aa89f54557abd4c903cc509b0061
Lucas Verron <luk.verron24@gmail.com>
2025-11-17 13:28:13 +0100

    étape 3

--- tp3/ex1_etoile.c
+++ tp3/ex1_etoile.c
@@ -7,19 +7,35 @@
 int main(){
     int size = 0;
     printf("Choissisez la taille de votre etoile ( 5<= taille <= 10 et taille soit un entier)\n");
-    scanf("%d",size);
+    scanf("%d",&size);
+    while (size < 5 || size > 10)
+    {
+        printf("valeur non conforme ( 5<= taille <= 10 et taille soit un entier)\n");
+        scanf("%d",&size); 
+    }
+    
     int width = 0;
     width = size * size;
     int max_height = 0;
     max_height = 3*size - 1;
     char img [110][30];
-    for(int i = 0 ; i < width ; i++)
+    for(int i = 0 ; i < max_height ; i++)
     {
-        for(int j = 0 ; j < max_height ; j++)
+        for(int j = 0 ; j < width ; j++)
         {
-            printf(" ");
+            img[j][i] = ' ';
+        }
+
+    }
+    for (int i = 0 ; i < max_height ; i++)
+    {
+        for(int j = 0 ; j < width ; j++)
+        {
+            printf("%c",img[i][j]);
         }
     }
+    
+    
 
 
 
