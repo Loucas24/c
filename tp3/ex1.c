e3bebaa13cc3e6253abe0c30b8c770d5474a4581
Lucas Verron <luk.verron24@gmail.com>
2025-11-17 13:05:55 +0100

    étape 2

--- tp3/ex1.c
+++ tp3/ex1.c
@@ -5,8 +5,12 @@
 
 
 int main(){
-    int taille = 0;
-    printf("Choissisez la taille de votre etoile ( 5<= taille <= 10)\n");
-    scanf("%d",taille);
+    int size = 0;
+    printf("Choissisez la taille de votre etoile ( 5<= taille <= 10 et taille soit un entier)\n");
+    scanf("%d",size);
+    int width = 0;
+    width = size * size;
+    int max_height = 0;
+    max_height = 3*size - 1;
 
 }
