689f135537937e4d180aaa343bd3b76f3e7106f1
Lucas Verron <luk.verron24@gmail.com>
2025-11-24 13:02:19 +0100

    ex1 etape2 tp4

--- tp4/ex1.c
+++ tp4/ex1.c
@@ -5,7 +5,7 @@
 
 void afficher_menu()
 {
-    printf("===== GESTION DES NOTES =====\n ");
+    printf("===== GESTION DES NOTES =====\n");
     printf("1. Saisir le nombre d'eleves\n");
     printf("2. Saisir les notes des eleves\n");
     printf("3. Afficher toutes les notes\n");
@@ -15,9 +15,17 @@ void afficher_menu()
     printf("0. Quitter\n");
 }
 
+int lire_choix()
+{
+    int choix = 0;
+    printf("Votre choix : ");
+    scanf("%d",choix);
+    return choix;
+}
 
 int main(){
     afficher_menu();
+    int choix = lire_choix();
     return 0;
 }
 
