fcfc873cfdbfa866bf6e2a9393d09f4c1ebc428a
Lucas Verron <luk.verron24@gmail.com>
2025-11-24 13:12:38 +0100

    ex1 etape3 tp4

--- tp4/ex1.c
+++ tp4/ex1.c
@@ -19,13 +19,35 @@ int lire_choix()
 {
     int choix = 0;
     printf("Votre choix : ");
-    scanf("%d",choix);
+    scanf("%d",&choix);
     return choix;
+}
+
+int saisirNombreEleves()
+{
+    int nb_eleve = 0;
+    printf("Entrez le nombre d'eleves (1 à 30) : ");
+    scanf("%d",&nb_eleve);
+    while (nb_eleve < 1 || nb_eleve > 30)
+    {
+        printf("Valeur invalide\n");
+        printf("Entrez le nombre d'eleves (1 à 30) : ");
+        scanf("%d",&nb_eleve);
+    }
+    return nb_eleve;
+    
 }
 
 int main(){
     afficher_menu();
     int choix = lire_choix();
+    switch (choix)
+    {
+        case 1 :
+        {
+            int nb_eleve = saisirNombreEleves();
+        }
+    }
     return 0;
 }
 
