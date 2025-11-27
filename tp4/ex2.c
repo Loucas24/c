fa9fe59d0cf2b3716c3ce09ac3697fcbd73783d9
Lucas Verron <luk.verron24@gmail.com>
2025-11-27 17:01:53 +0100

    ex2 ajout nombre_billets + correction montant valide

--- tp4/ex2.c
+++ tp4/ex2.c
@@ -36,9 +36,53 @@ void montant_valide(int montant)
         printf("Montant à retirer : ");
         scanf("%d",&montant);
     }
+    while (montant< 5)
+    {
+        printf("Erreur : montant invalide\n");
+        printf("Le montant doit etre entre 5 et 1000 euros, divisible par 5. ");
+        printf("Montant à retirer : ");
+        scanf("%d",&montant);
+    }
+    while (montant>1000)
+    {
+        printf("Erreur : montant invalide\n");
+        printf("Le montant doit etre entre 5 et 1000 euros, divisible par 5. ");
+        printf("Montant à retirer : ");
+        scanf("%d",&montant);
+    }
 }
 
-
+void nombre_billets(int montant)
+{
+    int indice = 0;
+    while (montant >= 50)
+    {
+        montant = montant - 50;
+        indice = indice+1;
+    }
+    printf("%d billets de 50 euros\n");
+    indice = 0;
+    while (montant >= 20)
+    {
+        montant = montant - 20;
+        indice = indice+1;
+    }
+    printf("%d billets de 20 euros\n");
+    indice = 0;
+    while (montant >= 10)
+    {
+        montant = montant - 10;
+        indice = indice+1;
+    }
+    printf("%d billets de 10 euros\n");
+    indice = 0;
+    while (montant >= 5)
+    {
+        montant = montant - 5;
+        indice = indice+1;
+    }
+    printf("%d billets de 5 euros\n");
+}
 
 
 
