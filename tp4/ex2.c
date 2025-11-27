c59778ecd6b9e97e0dbe79630f248f01ea8eac72
Lucas Verron <luk.verron24@gmail.com>
2025-11-27 16:44:59 +0100

    ex2 etape1 tp4

---
+++ tp4/ex2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+
+void afficher_menu()
+{
+    printf("=== BANQUE - DISTRIBUTEUR ===\n");
+    printf("1. Faire un retrait\n");
+    printf("2. Quitter\n");
+}
+
