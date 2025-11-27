7283b410b34d9aa706474bed462a8c73e669bc23
Lucas Verron <luk.verron24@gmail.com>
2025-11-27 16:55:21 +0100

    ex2 ajout montant_valide

--- tp4/ex2.c
+++ tp4/ex2.c
@@ -23,8 +23,19 @@ int saisir_montant()
     int montant = 0;
     printf("Montant à retirer : ");
     scanf("%d",&montant);
+    montant_valide(montant);
     return montant;
-    
+}
+
+void montant_valide(int montant)
+{
+    while (montant%5 != 0)
+    {
+        printf("Erreur : montant invalide\n");
+        printf("Le montant doit etre entre 5 et 1000 euros, divisible par 5. ");
+        printf("Montant à retirer : ");
+        scanf("%d",&montant);
+    }
 }
 
 
