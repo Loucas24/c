1e18f33dcdadae32369dfd9c180af602e8a6066b
Lucas Verron <luk.verron24@gmail.com>
2025-11-27 19:27:08 +0100

    ex2 enfin finiiiiiiiiiiiiiiiiii

--- tp4/ex2.c
+++ tp4/ex2.c
@@ -27,34 +27,14 @@ int saisir_montant(int montant)
 
 void montant_valide(int montant)
 {
-    if (montant >= 5 && montant <= 1000 && montant % 5 == 0)
+    while ( montant < 5 || montant%5 != 0 || montant > 1000)
     {
-        printf("Montant valide\n");
-    }
-    else
-    {
-        while (montant < 5)
-        {
-            printf("Erreur : montant invalide\n");
-            printf("Le montant doit etre entre 5 et 1000 euros, divisible par 5.\n");
-            printf("Montant a retirer : ");
-            scanf("%d",&montant);
-        }
-        while (montant%5 != 0)
-        {
-            printf("Erreur : montant invalide\n");
-            printf("Le montant doit etre entre 5 et 1000 euros, divisible par 5.\n");
-            printf("Montant a retirer : ");
-            scanf("%d",&montant);
-        }
-        while (montant > 1000)
-        {
-            printf("Erreur : montant invalide\n");
-            printf("Le montant doit etre entre 5 et 1000 euros, divisible par 5.\n");
-            printf("Montant a retirer : ");
-            scanf("%d",&montant);
-        }    
-    }
+        printf("Erreur : montant invalide\n");
+        printf("Le montant doit etre entre 5 et 1000 euros, divisible par 5.\n");
+        printf("Montant a retirer : ");
+        scanf("%d",&montant);
+    }  
+
 }
 
 void nombre_billets(int montant)
@@ -63,30 +43,28 @@ void nombre_billets(int montant)
     int indice_20 = 0;
     int indice_10 = 0;
     int indice_5 = 0;
-    while ((montant - 50) >= 0)
+    while (montant >= 50)
     {
         montant = montant - 50;
         indice_50 = indice_50 + 1;
-        printf("%d",indice_50);
     }
-    while ((montant - 20) >= 0)
+    while (montant >= 20)
     {
         montant = montant - 20;
-        indice_20  = indice_20 + 1;
-        printf("%d",indice_20);
+        indice_20 = indice_20 + 1;
     }
-    while ((montant - 10) >= 0)
+    while (montant >= 10)
     {
         montant = montant - 10;
         indice_10 = indice_10 + 1;
-        printf("%d",indice_10);
     }
-    while ((montant - 5) >= 0)
+    while (montant >= 5)
     {
         montant = montant - 5;
-        indice_5 = indice_5 +1;
-        printf("%d",indice_5);
+        indice_5 = indice_5 + 1;
     }
+    
+
     printf("%d billets de 50 euros\n",indice_50);
     printf("%d billets de 20 euros\n",indice_20);
     printf("%d billets de 10 euros\n",indice_10);
@@ -104,7 +82,7 @@ int main(){
     switch (choix)
     {
         case 1:
-            saisir_montant(montant);
+            montant = saisir_montant(montant);
             montant_valide(montant);
             nombre_billets(montant);
             break;
