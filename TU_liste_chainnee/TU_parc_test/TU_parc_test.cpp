d50c03e77c86b3647fdd4d2d316a9f77a8a2a944
Lucas Verron <luk.verron24@gmail.com>
2026-03-23 14:44:06 +0100

    test du sujet pour est pair corrigé

---
+++ TU_liste_chainnee/TU_parc_test/TU_parc_test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TUparctest
+{
+	TEST_CLASS(TUparctest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
--- tp4_tu/TP4_TU_test/TP4_TU_test.cpp
+++ tp4_tu/TP4_TU_test/TP4_TU_test.cpp
@@ -15,7 +15,23 @@ namespace TP4TUtest
 		
 		TEST_METHOD(TestEstPair)
 		{
-			Assert::AreEqual(estPair(6), 1);
+			Assert::AreEqual(estPair(4), 1);
+		}
+		TEST_METHOD(TestEstPair2)
+		{
+			Assert::AreEqual(estPair(7), 0);
+		}
+		TEST_METHOD(TestEstPair3)
+		{
+			Assert::AreEqual(estPair(0), 1);
+		}
+		TEST_METHOD(TestEstPair4)
+		{
+			Assert::AreEqual(estPair(-2), 1);
+		}
+		TEST_METHOD(TestEstPair5)
+		{
+			Assert::AreEqual(estPair(-3), 0);
 		}
 
 		TEST_METHOD(TestEstMax)
@@ -47,11 +63,6 @@ namespace TP4TUtest
 		TEST_METHOD(TestEstMax2)
 		{
 			Assert::AreEqual(max2(6, 11), 11);
-		}
-
-		TEST_METHOD(TestEstPair2)
-		{
-			Assert::AreEqual(estPair(6), 0);
 		}
 
 		TEST_METHOD(TestFactoriel2)
